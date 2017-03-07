f50d3973aaefafdd85c14a4dde8c6839c23fafe2
Sean Dennis <carter.swychord@gmail.com>
2017-03-07 17:42:32 -0600

    added value counter to slider. Final commit!

--- pyramid-slide.js
+++ pyramid-slide.js
@@ -1,14 +1,32 @@
-var height = document.getElementById("height").value;
-var symbol = document.getElementById("symbol").value;
+height = document.getElementById("height").value;
+default_symbol = "#"
+document.getElementById("construction").remove();
+how_high = slider_value
+how_high.innerText = height
+console.log(how_high)
 
 drawPyramid(height);
 
 
 
 function change_symbol(symbol) {
+  var previous_symbol = document.getElementById("symbol").value;
 
-  var pyramid_text = document.getElementById("pyramid");
-  var new_pyramid = pyramid_text.replace()
+  var pyramid_text = document.getElementById("pyramid").innerText;
+//  var new_pyramid = pyramid_text.replace()
+  console.log(pyramid_text);
+  console.log(default_symbol + "previous symbol");
+  new_symbol = document.getElementById("symbol").value;
+  console.log(new_symbol + "new symbol");
+
+  new_pyramid = document.getElementById("pyramid").innerText;
+  new_pyramid.replace(new_symbol, default_symbol)
+
+  default_symbol = new_symbol;
+  height = document.getElementById("height").value;
+  drawPyramid(height)
+//  new_pyramid = pyramid_text.replace(current_symbol, new_symbol)
+
 }
 
 
@@ -20,6 +38,8 @@ function change_symbol(symbol) {
  * Renders, in the HTML document, a Mario pyramid of the specified height
  */
 function drawPyramid(height) {
+  how_high = slider_value
+  how_high.innerText = height
 
 var current_pyramid = document.getElementById("pyramid");
 current_pyramid.innerText = ""
@@ -39,10 +59,10 @@ current_pyramid.innerText = ""
         // build up a string for this row
         var rowStr = "";
         for (var i = 0; i < numSpaces; i++) {
-            rowStr += "."; // QUIZ: what happens if we use a space (" ") instead of a period?
+            rowStr += "&nbsp;" // QUIZ: what happens if we use a space (" ") instead of a period?
         }
         for (var i = 0; i < numBricks; i++) {
-            rowStr += "#";
+            rowStr += default_symbol;
         }
 
         // you can delete this now
@@ -58,7 +78,7 @@ current_pyramid.innerText = ""
 
 
         var text = document.createElement("p");
-        text.innerText = rowStr;
+        text.innerHTML = rowStr;
         document.getElementById("pyramid").appendChild(text)
     }
 }
